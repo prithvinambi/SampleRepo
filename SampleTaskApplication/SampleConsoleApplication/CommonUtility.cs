61b166f2cb14fc33c1e1fe5af9c624156178f611
prithvinambi <nambiprithviteja@gmail.com>
2015-11-05 15:48:25 +0530

    Sample Console Application added

---
+++ SampleTaskApplication/SampleConsoleApplication/CommonUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleConsoleApplication
+{
+    public class CommonUtility
+    {
+        public static void CommonUtilMethod() 
+        {
+            Console.WriteLine("!!!!!!!!!!!!!! ----------------- !!!!!!!!!!!!!!");
+            Console.WriteLine("Output from SampleConsoleApp Util method");
+            Console.WriteLine("!!!!!!!!!!!!!! ----------------- !!!!!!!!!!!!!!");
+        }
+    }
+}
---
+++ SampleTaskApplication/SampleConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************ ----------------- **************");
+            Console.WriteLine("Output from Sample Console Application");
+            Console.WriteLine("************ ----------------- **************");
+            Console.ReadKey();
+        }
+    }
+}
